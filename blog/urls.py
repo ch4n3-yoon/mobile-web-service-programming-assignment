3cead372a6496333a6fa1cbbef7045b06303fa80
Seokchan Yoon <scyoon@stealien.com>
2023-05-27 22:10:35 +0900

    Add some functions (write, edit, view) for PostModel

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from blog.views import post_list
+from blog.views import post_detail
+from blog.views import post_new
+from blog.views import post_edit
 from blog.views import test
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit', post_edit, name='post_edit'),
     path('test/', test, name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,63 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.http import HttpResponse
+
+from blog.models import Post as PostModel
+from blog.forms import PostForm
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = PostModel.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'user': request.user, 'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(PostModel, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method != 'POST':
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+    if not request.user:
+        return HttpResponse('Unauthorized', status=401)
+
+    form = PostForm(request.POST)
+    if not form.is_valid():
+        return HttpResponse('Invalid form', status=400)
+
+    post = form.save(commit=False)
+    post.author = request.user
+    post.published_date = timezone.now()
+    post.save()
+
+    return redirect('post_detail', pk=post.pk)
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(PostModel, pk=pk)
+    if request.method != 'POST':
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+    if not request.user:
+        return HttpResponse('Unauthorized', status=401)
+
+    form = PostForm(request.POST, instance=post)
+
+    if not form.is_valid():
+        return HttpResponse('Invalid form', status=400)
+
+    post = form.save(commit=False)
+    post.author = request.user
+    post.published_date = timezone.now()
+    post.save()
+
+    return redirect('post_detail', pk=post.pk)
 
 
 def test(request):
